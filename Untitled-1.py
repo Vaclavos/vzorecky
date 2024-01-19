92a23930451df0e498702a4cd4b3c9887b2ed3c3
Vaclavos <vaclav.hruska@student.souepl.cz>
2024-01-19 12:27:18 +0100

    vzorecky ap

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+print("Vítejte v apikaci pro výpočet obvodu obdélníku")
+
+
+
+r = input("Zadejte délku poloměru:")
+
+
+r = int(r)
+
+
+
+if r>0:
+    print("Výsledek je")
+    print(3.14*r**2)
+
+else:
+    print("Co to deláš")                                                    
